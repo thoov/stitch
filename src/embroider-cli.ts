577c7cff8cb9e0ea1defe1df15621ff442b7076f
Steve Calvert <scalvert@linkedin.com>
2021-05-21 10:23:28 -0700

    chore: Adding yargs and appropriate commands

--- src/embroider-cli.ts
+++ src/embroider-cli.ts
@@ -1,3 +1,43 @@
+import * as yargs from 'yargs';
+
 export async function run(argv: string[] = process.argv.slice(2)) {
-  console.log('Running cli');
+  let parser = yargs
+    .scriptName('embroider-cli')
+    .usage(
+      `
+  A compatabilty and migration CLI for Embroider
+  embroider-cli <command> [options]`
+    )
+    .command({
+      command: 'preflight',
+      aliases: ['p'],
+      describe: 'Runs an Embroider preflight check',
+      builder: (yargs) => {
+        return yargs.usage('embroider-cli preflight [options]').options({});
+      },
+      handler: async (argv: yargs.Arguments) => {
+        console.log('Preflight check commencing. Prepare for liftoff.');
+      },
+    })
+    .command({
+      command: 'migrate',
+      aliases: ['m'],
+      describe: 'Runs an Embroider migration',
+      builder: (yargs) => {
+        return yargs.usage('embroider-cli migrate [options]').options({});
+      },
+      handler: async (argv: yargs.Arguments) => {
+        console.log('Migration commencing. Hold on to your hats.');
+      },
+    })
+    .showHelpOnFail(false)
+    .wrap(yargs.terminalWidth())
+    .help()
+    .version();
+
+  if (argv.length === 0) {
+    parser.showHelp();
+  } else {
+    parser.parse(argv);
+  }
 }
