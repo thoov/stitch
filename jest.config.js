bc88310a220afde3e220b8ec7f58e56482227262
Steve Calvert <scalvert@linkedin.com>
2021-05-23 11:23:47 -0700

    fix: Missing semi

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
     '\\.ts$': 'ts-jest',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  modulePathIgnorePatterns: ['<rootDir>/lib']
+  modulePathIgnorePatterns: ['<rootDir>/lib'],
   testPathIgnorePatterns: ['/__utils__/', '/__fixtures__/'],
 };
