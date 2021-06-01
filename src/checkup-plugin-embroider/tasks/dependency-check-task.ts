bf4f025d6713e161b3ab348ae3559dfe76923b94
Steve Calvert <scalvert@linkedin.com>
2021-06-01 07:49:36 -0700

    chore: Enable stitch to pass arguments to tasks

--- src/checkup-plugin-embroider/tasks/dependency-check-task.ts
+++ src/checkup-plugin-embroider/tasks/dependency-check-task.ts
@@ -14,13 +14,18 @@ export default class DependencyCheck extends BaseTask implements Task {
   category = 'embroider';
 
   async run(): Promise<Result[]> {
-    const packageCompatibility: PackageCheck  = packageCompatibilityJSON;
+    debugger;
+    const packageCompatibility: PackageCheck = packageCompatibilityJSON;
     const pkgs = await findBadPackages(this.context.options.cwd, packageCompatibility);
     const results = [];
 
     for (const pkg of pkgs) {
       results.push({
-        message: { text: `Detected version of ${pkg.packageName} as ${pkg.packageVersion} but ${packageCompatibility[pkg.packageName]} found in ${pkg.packageBreadcrumb.join(' > ')}.` },
+        message: {
+          text: `Detected version of ${pkg.packageName} as ${pkg.packageVersion} but ${
+            packageCompatibility[pkg.packageName]
+          } found in ${pkg.packageBreadcrumb.join(' > ')}.`,
+        },
         ruleId: this.taskName,
         properties: {
           taskDisplayName: this.taskDisplayName,
@@ -28,8 +33,8 @@ export default class DependencyCheck extends BaseTask implements Task {
           stitchResult: {
             packageName: pkg.packageName,
             packageVersion: pkg.packageVersion,
-            packageBreadcrumb: pkg.packageBreadcrumb
-          }
+            packageBreadcrumb: pkg.packageBreadcrumb,
+          },
         },
       });
     }
