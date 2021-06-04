2ad13d8a35fa8b9542042d2febd276d73a7aec68
Travis Hoover <thoov7@gmail.com>
2021-06-04 12:02:38 -0700

    adding basic transform for ember-cli-build.js

--- src/codemods/transforms/setup/index.js
+++ src/codemods/transforms/setup/index.js
@@ -1,19 +1,65 @@
 const { getParser } = require('codemod-cli').jscodeshift;
-const { getOptions } = require('codemod-cli');
 
+// https://astexplorer.net/#/gist/abbbde24e6599b8f0a7aa7e37416bf7e/dd15c330695ea3f53779aba07bfdfaeb77b0f238
 module.exports = function transformer(file, api) {
   const j = getParser(api);
-  const options = getOptions();
+  let root = j(file.source);
 
-  return j(file.source)
-    .find(j.Identifier)
-    .forEach(path => {
-      path.node.name = path.node.name
-        .split('')
-        .reverse()
-        .join('');
-    })
-    .toSource();
+  // TODO: make sure we only work on the correct files
+
+  let returnStatments = root.find(j.ReturnStatement);
+  let appToTreeCallExpression = root.find(j.CallExpression, {
+    callee: {
+      object: {
+        name: 'app'
+      },
+
+      property: {
+        name: 'toTree'
+      }
+    }
+  });
+
+  // Comments might be attached to the return statement so we nned to save them
+  // and reattach them after we apply the transform.
+  let comments = returnStatments.get(0).node.comments;
+
+  returnStatments.replaceWith((path) => {
+    // this checks if the return statement is `return app.toTree();`
+    if (appToTreeCallExpression.nodes()[0] !== path.node.argument) {
+      return path.node;
+    }
+
+    return [
+      j.variableDeclaration('const', [
+        j.variableDeclarator(
+          j.objectPattern([
+      		j.objectProperty(
+              j.identifier('Webpack'),
+              j.identifier('Webpack'),
+              false,
+              true
+            )
+    	  ]),
+          j.callExpression(
+            j.identifier('require'), [j.stringLiteral('@embroider/webpack')])
+        )
+      ]),
+      j.variableDeclaration('const', [
+        j.variableDeclarator(j.identifier('tree'), j.callExpression(j.memberExpression(
+          j.callExpression(j.identifier('require'), [j.literal('@embroider/compat')]),
+          j.identifier('compatBuild'),
+          false
+        ), [j.identifier('app'), j.identifier('Webpack')]))
+      ]),
+
+      j.returnStatement(j.identifier('tree'))
+    ];
+  });
+
+  returnStatments.get(0).node.comments = comments;
+
+  return root.toSource({quote: 'single'});
 };
 
 module.exports.type = 'js';
