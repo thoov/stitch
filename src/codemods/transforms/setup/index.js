e3da4afd854133ea83cd5f4945369fa5d7943f4b
Travis Hoover <thoov7@gmail.com>
2021-06-05 10:01:07 -0700

    handle extra trees case

--- src/codemods/transforms/setup/index.js
+++ src/codemods/transforms/setup/index.js
@@ -6,7 +6,6 @@ module.exports = function transformer(file, api) {
   let root = j(file.source);
 
   // TODO: make sure we only work on the correct files
-
   let returnStatments = root.find(j.ReturnStatement);
   let appToTreeCallExpression = root.find(j.CallExpression, {
     callee: {
@@ -23,6 +22,11 @@ module.exports = function transformer(file, api) {
   // Comments might be attached to the return statement so we nned to save them
   // and reattach them after we apply the transform.
   let comments = returnStatments.get(0).node.comments;
+
+  let extraTrees = [];
+  if (appToTreeCallExpression.nodes()[0]) {
+    extraTrees = appToTreeCallExpression.nodes()[0].arguments;
+  }
 
   returnStatments.replaceWith((path) => {
     // this checks if the return statement is `return app.toTree();`
@@ -45,13 +49,8 @@ module.exports = function transformer(file, api) {
             j.identifier('require'), [j.stringLiteral('@embroider/webpack')])
         )
       ]),
-      j.variableDeclaration('const', [
-        j.variableDeclarator(j.identifier('tree'), j.callExpression(j.memberExpression(
-          j.callExpression(j.identifier('require'), [j.literal('@embroider/compat')]),
-          j.identifier('compatBuild'),
-          false
-        ), [j.identifier('app'), j.identifier('Webpack')]))
-      ]),
+	  compatBuildStatement(j, extraTrees),
+
 
       j.returnStatement(j.identifier('tree'))
     ];
@@ -59,7 +58,51 @@ module.exports = function transformer(file, api) {
 
   returnStatments.get(0).node.comments = comments;
 
+  if (extraTrees.length > 0) {
+    let compatBuild = root.find(j.CallExpression, {
+      callee: {
+        object: {
+          callee: {
+            name: 'require'
+          }
+        },
+
+        property: {
+          name: 'compatBuild'
+        }
+      }
+    }).filter(path => {
+      return path.value.arguments[0].name === 'app' && path.value.arguments[1].name === 'Webpack';
+    });
+
+    // TODO: clean this up with less magic numbers
+  	compatBuild.nodes()[0].arguments[2].properties[0].value = extraTrees[0];
+  }
+
   return root.toSource({quote: 'single'});
-};
+}
+
+function compatBuildStatement(j, extraTrees) {
+  if (extraTrees.length > 0) {
+    return j.variableDeclaration('const', [
+      j.variableDeclarator(j.identifier('tree'), j.callExpression(j.memberExpression(
+        j.callExpression(j.identifier('require'), [j.literal('@embroider/compat')]),
+        j.identifier('compatBuild'),
+        false
+      ), [j.identifier('app'), j.identifier('Webpack'), j.objectExpression([
+        j.property('init', j.identifier('extraPublicTrees'), j.arrayExpression([]))
+      ])]))
+    ]);
+  }
+
+  return j.variableDeclaration('const', [
+    j.variableDeclarator(j.identifier('tree'), j.callExpression(j.memberExpression(
+      j.callExpression(j.identifier('require'), [j.literal('@embroider/compat')]),
+      j.identifier('compatBuild'),
+      false
+    ), [j.identifier('app'), j.identifier('Webpack')]))
+  ]);
+}
+
 
 module.exports.type = 'js';
